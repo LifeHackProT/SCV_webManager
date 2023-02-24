88ff3bab9cd507e15a95f105a2114d0c2e668bf8
snslab <ha6948@cu.ac.kr>
2023-02-24 16:27:24 +0900

    2023.02.24 마커이동오류 발생 시 새로고침

--- gps_client/src/Kakaomap.js
+++ gps_client/src/Kakaomap.js
@@ -1,17 +1,9 @@
 /* global kakao */
 import React, { useEffect, useState } from 'react';
 import ROSLIB from 'roslib';
-import { MapMarker, Map } from "react-kakao-maps-sdk";
+import ROS3D from 'roslib';
+import { MapMarker, Map, Polyline } from "react-kakao-maps-sdk";
 const Kakaomap = () => {
-
-    //
-    //     //마커 위도 경도 표시
-    //     kakao.maps.event.addListener(map, 'marker_changed', function() {
-    //         var message = '<p>중심 좌표는 위도 ' + marker.getPosition().getLat() + ', 경도 ' + marker.getPosition().getLng() + '입니다</p>';
-    //         var resultDiv = document.getElementById('result');
-    //         resultDiv.innerHTML = message;
-    //     });
-    //
 
     //지도 중심좌표
     const [state, setState] = useState({
@@ -24,14 +16,19 @@ const Kakaomap = () => {
     })
 
     //웹소켓 사용 ROSLIB websocket
+    var rosbridge_url = 'ws://localhost:9090';
+
     var ros = new ROSLIB.Ros({
-        url: 'ws://localhost:9090'
+        url: rosbridge_url
     });
+
+    //ros 연결 상태 출력
     ros.on('connection', function(){
         console.log('Connected to websocket server.');
     });
     ros.on('error', function(error){
         console.log('Error connecting to websocket server: ', error);
+        window.location.reload(); //오류뜰 때 사이트 새로고침
     });
     ros.on('close', function(){
         console.log('Connection to websocket server closed.');
@@ -44,33 +41,70 @@ const Kakaomap = () => {
         messageType : "sensor_msgs/NavSatFix"
     });
 
+    //velodyne 연결
+    var viewer = new ROS3D.Viewer({
+        divID : 'viewer',
+        width : 800,
+        height : 600,
+        antialias : true
+    });
+
+    var tfClient = new ROSLIB.TFClient({
+        ros : ros,
+        angularThres : 0.01,
+        transThres : 0.01,
+        rate : 10.0,
+        fixedFrame : '/velodyne'
+    });
+
+    //Create Kinect scene node
+    var kinectNode = new ROS3D.SceneNode({
+        frameID : 'velodyne',
+        tfClient : tfClient,
+        object : PointCloud2
+    });
+
     //마커 위도경도
-    const [markLat, setMarkLat] = useState();
-    const [markLng, setMarkLng] = useState();
+    const [markLoc, setMarkLoc] = useState([{
+        lat : state.center.lat,
+        lng : state.center.lng,
+    }]);
+
+    //마커 이동 선 표시
+    const [line, setLine] = useState ([{
+        lat : state.center.lat,
+        lng : state.center.lng,
+    },])
 
     useEffect(
         () => {
             //rosbag 메세지 콘솔창 출력
             listener.subscribe(function(message) {
                 console.log('Received message on' + listener.name
-                    + '\nlongtitude : ' + message.longitude
-                    + '\nlatitude : ' + message.latitude,
+                    + '\nlatitude : ' + message.latitude
+                    + '\nlongtitude : ' + message.longitude,
                 );
-                //마커 위도경도 변화
-                setMarkLat(message.latitude);
-                setMarkLng(message.longitude);
+                //마커 위도경도 변경
+                setMarkLoc({
+                    lat: message.latitude, lng: message.longitude
+                })
                 //중심 좌표 이동
                 setState({
                     center: { lat: message.latitude, lng: message.longitude }
                 })
+                // //선 표시
+                // const onCreate = () => {
+                //     const newLine = {
+                //         lat: message.latitude,
+                //         lng: message.longitude,
+                //     };
+                //     setLine([...line, newLine]);
+                // }
             });
             return () => {
                 listener.unsubscribe();
             }
-        },
-        []
-    )
-
+        },)
 
     return (
         <div>
@@ -86,14 +120,22 @@ const Kakaomap = () => {
                 }}
                 level={2} // 지도의 확대 레벨
             >
-                <MapMarker position={{
-                    lat: markLat,
-                    lng: markLng,
+                <MapMarker position={{ //마커 표시
+                    lat: markLoc.lat,
+                    lng: markLoc.lng,
                 }}></MapMarker>
+                <Polyline path = {[//마커 이동 선 표시
+                    { lat: line.lat, lng: line.lng },
+                ]}
+                          strokeWeight = {3} //선 두께
+                         strokeColor = {"#39DE2A"} //선 색깔
+                         strokeOpacity = {0.8} //선의 불투명도
+                         strokeStyle = {"solid"} //선의 스타일
+                ></Polyline>
             </Map>
             <h1>마커 위치</h1>
-            <h1>{'위도 : ' + markLat}</h1>
-            <h1>{'경도 : ' + markLng}</h1>
+            <h1>{'위도 : ' + markLoc.lat}</h1>
+            <h1>{'경도 : ' + markLoc.lng}</h1>
         </div>
     );
 };
